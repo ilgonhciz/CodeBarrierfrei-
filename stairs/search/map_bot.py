b51171902a3c163e16f6f30682da2e959a8544a7
Kai Eberl <>
2021-06-20 08:19:00 +0200

    Implemented API calls for information about position, website and social media links

--- stairs/search/map_bot.py
+++ stairs/search/map_bot.py
@@ -8,27 +8,31 @@ def get_toilets(lat, lon):
 
 
 def main():
-    query = 'Sushi Sano München'
-    nom_url = 'https://nominatim.openstreetmap.org/search?q={}&limit=1&extratags=1&format=json'.format(query)
+    query = 'Lilly Ruby München'
+    nom_url = 'https://nominatim.openstreetmap.org/search?q={}&limit=3&extratags=1&format=json'.format(query)
     json_obj = get_json(nom_url)
 
     lat = json_obj[0]['lat']
     lon = json_obj[0]['lon']
-    place_id = json_obj[0]['place_id']
     try:
-        website = json_obj[0]['extratags']['website']
+        website = json_obj[0]['extratags']['contact:website']
     except KeyError:
         website = ''
     try:
         wheel = json_obj[0]['extratags']['wheelchair']
     except KeyError:
         wheel = ''
+    fb_link = json_obj[0]['extratags']['contact:facebook']
+    tw_link = json_obj[0]['extratags']['contact:twitter']
+    insta_link = json_obj[0]['extratags']['contact:instagram']
 
-    osm_url = 'https://www.openstreetmap.org/api/0.6/node/{}.json'.format(place_id)
-    json_obj = get_json(osm_url)
-    fb_link = json_obj['elements']['tags']['contact:facebook']
-    tw_link = json_obj['elements']['tags']['contact:twitter']
-    insta_link = json_obj['elements']['tags']['contact:instagram']
+    get_website_images()
+
+    # osm_url = 'https://www.openstreetmap.org/api/0.6/node/{}.json'.format(place_id)
+    # json_obj = get_json(osm_url)
+    # fb_link = json_obj['elements']['tags']['contact:facebook']
+    # tw_link = json_obj['elements']['tags']['contact:twitter']
+    # insta_link = json_obj['elements']['tags']['contact:instagram']
 
 
 def get_json(url):
@@ -37,6 +41,10 @@ def get_json(url):
     if response.ok:
         json_obj = response.json()
     return json_obj
+
+
+def get_website_images():
+    pass
 
 
 if __name__ == '__main__':
